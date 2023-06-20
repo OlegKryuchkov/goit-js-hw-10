9528b04804965d6c3daffba9f9f5665ae6bc0bcd
OlegKryuchkov <kryuchkov.oleg@gmail.com>
2023-06-20 21:53:31 +0300

    Update

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -7,10 +7,4 @@ function fetchBreeds() {
   );
 }
 
-function fetchCatByBreed(breedId) {
-  return fetch(
-    `https://api.thecatapi.com/v1/images/search?limit=1&breed_ids=${breedId}&api_key=${API_key}`
-  ).then(data => data.json());
-}
-
 export { fetchBreeds, fetchCatByBreed };
